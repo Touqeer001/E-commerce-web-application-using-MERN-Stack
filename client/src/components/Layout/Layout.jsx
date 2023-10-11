88d462b9e7997c806b434713cb80e2f26f5b44c6
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-11 13:09:10 +0530

    update layout

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,18 +1,19 @@
 //rafce react arrow function with component
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const Layout = (props) => {
   return (
     <>
       <Header />
-      <main style={{ minHeight: "70vh" }} >
+      <main style={{ minHeight: "70vh" }}>
+        <ToastContainer />
+
         {props.children}
-   
       </main>
       <Footer />
     </>
