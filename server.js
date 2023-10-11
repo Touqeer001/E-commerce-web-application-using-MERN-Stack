a5ba0c2e177d892f083e6fb03e7705c09fa3d067
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-11 13:08:53 +0530

    for backend

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+//configure env
+dotenv.config();
+
+
+//rest object
+const app = express();
+
+
+app.use(express.json());
+
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(
+      `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+        .white
+    );
