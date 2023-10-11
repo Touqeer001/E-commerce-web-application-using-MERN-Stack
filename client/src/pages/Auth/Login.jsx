2ab5eac0e2aed1faea5f3ba98d7ede0b41e43ccb
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-11 13:37:10 +0530

    login componet

---
+++ client/src/pages/Auth/Login.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import Layout from "../../components/Layout/Layout";
+import { toast } from "react-toastify";
+
+const Login = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  //Form  Function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, address, phone);
+    toast.success("Register successfuly...");
+  };
+  return (
+    <>
+      <Layout>
+        <div className="row registers">
+          <div className="col-md-6">
+            <img id="image" src="/images/login.png" alt="Login"></img>
+          </div>
+
+          <div className="col-md-4 registerBorder">
+            <div className="registers">
+              <h3 style={{ color: "white" }}>Login Form</h3>
+              <form onSubmit={handleSubmit}>
+                {/* <div className="mb-3"> */}
+                {/* <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-contro"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter Email"
+                  />
+                </div> */}
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Email"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Enter Password"
+                  />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default Login;
