3b33779fc2d7900e85d899d988029e57c599c5b2
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-03 23:06:09 +0530

    changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Layout from "../components/Layout/Layout";
 const Home = () => {
   return (
     <Layout>
-      <h1 style={{ color: "white" }}>Home Page....Coming Soon</h1>
+      <h1 style={{ color: "white" }}>Home Page.....Coming Soon</h1>
     </Layout>
   );
 };
