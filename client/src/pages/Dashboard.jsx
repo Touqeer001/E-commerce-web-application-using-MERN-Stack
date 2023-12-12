01f9ac31cb5dee87ced1ed17fd4faca8fcda8813
Touqeer Ansari <touqeeransari001@gmail.com>
2023-12-12 22:04:07 +0530

    Update

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      <h1 style={{ color: "white" }}>Dashbord Coming soon</h1>
+    </Layout>
+  );
+};
+
+export default Dashboard;
