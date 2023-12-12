66258372774fc1541a7f07851bda3923ddb66498
Touqeer Ansari <touqeeransari001@gmail.com>
2023-12-12 22:03:43 +0530

    Update

---
+++ client/src/pages/Admin.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const Admin = () => {
+  return <Layout>
+  <h1 style={{ color: "white" }}>Admin Coming soon</h1>
+</Layout>;
+};
+
+export default Admin;
