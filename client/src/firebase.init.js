9dc662f85c39b42e288cc1236427e4222d4d6f14
Touqeer Ansari <touqeeransari001@gmail.com>
2023-12-14 22:19:52 +0530

    Changes

--- client/src/firebase.init.js
+++ client/src/firebase.init.js
@@ -1,29 +1,7 @@
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
- import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC5msF-WzbBTwmASKDE9NrjJX8NOtMp0dc",
-//   authDomain: "twiter-clone-c0d20.firebaseapp.com",
-//   projectId: "twiter-clone-c0d20",
-//   storageBucket: "twiter-clone-c0d20.appspot.com",
-//   messagingSenderId: "891518998974",
-//   appId: "1:891518998974:web:4937919aa5c2964823c1ef",
-
-//   measurementId: "G-V1YL8XZXHE",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// // const analytics = getAnalytics(app);
-// const auth = getAuth(app);
-// export default auth;
-
+import { getAuth } from "firebase/auth";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -40,7 +18,7 @@ const firebaseConfig = {
   storageBucket: "e-commerce-10a3f.appspot.com",
   messagingSenderId: "195331215849",
   appId: "1:195331215849:web:22b534f42336a17b4b7b78",
-  measurementId: "G-H4P0K1VH1P"
+  measurementId: "G-H4P0K1VH1P",
 };
 
 // Initialize Firebase
