1997b653a9d8eac9f4a2bf41a435eb7c1f83f814
Touqeer Ansari <touqeeransari001@gmail.com>
2023-12-17 23:16:30 +0530

    changes

--- client/src/components/Layout/Footer.jsx
+++ client/src/components/Layout/Footer.jsx
@@ -5,10 +5,7 @@ const Footer = () => {
   return (
     <>
       <div className="footer">
-        <h5 className="text-center">
-          {/* All Right Reserved &copy; toqeer_Ibn_Khursheed....*/}
-          All Right Reserve &copy;Team_05
-        </h5>
+        <h5 className="text-center">All Right Reserve &copy;Team_05</h5>
         <p className="text-center mt-3">
           <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
