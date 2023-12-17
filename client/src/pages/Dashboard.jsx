15d8d24594c422206b9866b19f9243068c9f4a2a
Touqeer Ansari <touqeeransari001@gmail.com>
2023-12-17 23:16:37 +0530

    changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import Layout from "../components/Layout/Layout";
 const Dashboard = () => {
   return (
     <Layout>
-      <h1 style={{ color: "white" }}>Dashbord Coming soon..</h1>
+      <h1 style={{ color: "white" }}>Dashbord Coming soon</h1>
     </Layout>
   );
 };
