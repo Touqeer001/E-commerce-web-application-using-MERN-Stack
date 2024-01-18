3d3da121ec9b4765b3d2fba356d203cb8f7a510c
Touqeer Ansari <touqeeransari001@gmail.com>
2024-01-18 21:19:30 +0530

    added Admin Dashbord

---
+++ client/src/pages/Admin/AdminDashbord.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+export const AdminDashbord = () => {
+  return (
+    <Layout>
+      <h1>AdminDashbord</h1>
+    </Layout>
+  );
+};
+export default AdminDashbord;
