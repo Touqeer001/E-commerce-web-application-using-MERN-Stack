0ac4180eff278e9b542454de295f38659dd3cfc6
Touqeer Ansari <touqeeransari001@gmail.com>
2024-01-19 19:31:02 +0530

    updated

--- client/src/pages/user/Dashboard.jsx
+++ client/src/pages/user/Dashboard.jsx
@@ -1,64 +1,35 @@
-// import React from "react";
-// import Layout from "../../components/Layout/Layout";
-// import auth from "../../firebase.init";
-// import UserMenu from "../../components/Layout/UserMenu";
-
-// const Dashboard = () => {
-//   return (
-//     <Layout>
-//       <div className="container-flui m-3 p-3 dashboard">
-//         <div className="row">
-//           <div className="col-md-3">
-//             <UserMenu />
-//           </div>
-//           <div className="col-md-9">
-//             <div className="card w-75 p-3">
-//               <h3>{auth?.user?.name}</h3>
-//               <h3>{auth?.user?.email}</h3>
-//               <h3>{auth?.user?.address}</h3>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </Layout>
-//   );
-// };
-
-// export default Dashboard;
-
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Layout from "../../components/Layout/Layout";
 import auth from "../../firebase.init";
 import UserMenu from "../../components/Layout/UserMenu";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Dashboard = () => {
-  const [userData, setUserData] = useState(null);
-
-  useEffect(() => {
-    // Assuming you have a 'users' collection in Firestore
-    const userDoc = auth?.user ? firebase.firestore().collection('users').doc(auth.user.uid) : null;
-
-    if (userDoc) {
-      userDoc.get().then((doc) => {
-        if (doc.exists) {
-          setUserData(doc.data());
-        }
-      });
+  const [userAuth] = useAuthState(auth);
+  
+  const truncateUsername = (username, maxLength) => {
+    if (!username) {
+      return ""; // or handle the case where username is null or undefined
     }
-  }, [auth?.user]);
-
+    if (username.length <= maxLength) {
+      return username;
+    } else {
+      return username.slice(0, maxLength) + "...";
+    }
+  };
   return (
     <Layout>
-      <div className="container-fluid m-3 p-3 dashboard">
+      <div className="container-flui m-3 p-3 dashboard">
         <div className="row">
           <div className="col-md-3">
             <UserMenu />
           </div>
           <div className="col-md-9">
             <div className="card w-75 p-3">
-              <h3>{userData?.name}</h3>
-              <h3>{auth?.user?.email}</h3>
-              <h3>{userData?.address}</h3>
+             
+              <h3>{`UserName:${truncateUsername(userAuth.displayName, 8)}`}</h3>
+              <h3>{`UserEmail:${userAuth.email}`}</h3>
+             
             </div>
           </div>
         </div>
