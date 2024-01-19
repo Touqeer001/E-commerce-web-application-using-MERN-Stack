741a264e17e6ae69be289f028d1cc59e6864e94a
Touqeer Ansari <touqeeransari001@gmail.com>
2024-01-19 19:30:17 +0530

    updated admin Dashboard

--- client/src/pages/Admin/AdminDashbord.jsx
+++ client/src/pages/Admin/AdminDashbord.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import auth from "../../firebase.init";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export const AdminDashbord = () => {
+  const [userAuth] = useAuthState(auth);
+  const truncateUsername = (username, maxLength) => {
+    if (!username) {
+      return ""; // or handle the case where username is null or undefined
+    }
+    if (username.length <= maxLength) {
+      return username;
+    } else {
+      return username.slice(0, maxLength) + "...";
+    }
+  };
   return (
     <Layout>
-      <h1>AdminDashbord</h1>
+      <div className="container-fluid m-3 p-3 dashboard">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">
+            <div className="card w-75 p-3">
+              {/* <h3> Admin Name : {auth?.user?.name}</h3>
+              <h3> Admin Email : {auth?.user?.email}</h3>
+              <h3> Admin Contact : {auth?.user?.phone}</h3> */}
+                 
+                 <h3>{`Admin Name:${truncateUsername(userAuth.displayName)}`}</h3>
+              <h3>{`Admin Email:${userAuth.email}`}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
