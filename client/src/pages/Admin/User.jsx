1b278fdec762e1e678cd858cbca57a294c820224
Touqeer Ansari <touqeeransari001@gmail.com>
2024-01-20 22:30:42 +0530

    initial commits

--- client/src/pages/Admin/User.jsx
+++ client/src/pages/Admin/User.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import Layout from "../../components/Layout/Layout";
 
 const User = () => {
   return (
-    <div>User</div>
-  )
-}
+    <Layout>
+      <div className="container-fluid m-3 p-3 dashboard">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu></AdminMenu>
+          </div>
+          <div className="col-md-9">
+            <h1 style={{ color: "white" }}>USer Related info</h1>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-export default User
+export default User;
