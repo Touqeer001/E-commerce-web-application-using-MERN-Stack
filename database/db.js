2eaba07e1cc43202b9192a6f2adbe2a4fd11f80f
Touqeer Ansari <touqeeransari001@gmail.com>
2024-01-25 23:39:33 +0530

    initial commit

--- database/db.js
+++ database/db.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
 const Connection = async () => {
-<<<<<<< HEAD
   const URL = `mongodb+srv://touqeer:Ansari@cluster0.9bh5jgq.mongodb.net/?retryWrites=true&w=majority`;
-=======
-  const URL = `Upload MONGODB URL`;
->>>>>>> 26d772683594c32516c09e97b913c9dfd61cd3e3
 
   try {
     await mongoose.connect(URL, { useNewUrlParser: true });
