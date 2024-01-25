94a5ff39d1572235065fa4d551f6eb9782deb68d
Touqeer Ansari <touqeeransari001@gmail.com>
2024-01-25 23:39:14 +0530

    updated

---
+++ Controllers/CategoryController.js
@@ -0,0 +1,36 @@
+import slugify from "slugify";
+import categoryModel from "../Model/categoryModel.js";
+
+export const createCategoryController = async (req, res) => {
+  try {
+    const { displayName } = req.user;
+    console.log("Request Body:", req.body);
+
+    if (!name) {
+      return res.status(401).send({ message: "Name is required" });
+    }
+    const existingCategory = await categoryModel.findOne({ name });
+    if (existingCategory) {
+      return res.status(200).send({
+        success: false,
+        message: "Category Already Exisits",
+      });
+    }
+    const category = await new categoryModel({
+      name,
+      slug: slugify(name),
+    }).save();
+    res.status(201).send({
+      success: true,
+      message: "new category created",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in Category",
+    });
+  }
+};
