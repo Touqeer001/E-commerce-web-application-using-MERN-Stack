65fd7bc7145c3aace97dd60b92bab27fc55a451c
Touqeer Ansari <touqeeransari001@gmail.com>
2024-01-30 19:13:53 +0530

    Updated Policy.jsx

--- client/src/pages/Policy.jsx
+++ client/src/pages/Policy.jsx
@@ -10,8 +10,23 @@ const Policy = () => {
         </div>
 
         <div className="col-md-4">
-          <p className="text-justify mt-2">
-            Regardin Policies and privacy.Coming Soon....
+          {/* <p className="text-justify mt-2 textDesign"> */}
+          <p>
+            <h1>Privacy Policy:</h1>
+            <hr></hr>
+          </p>
+          <h3 style={{ color: "white" }}></h3>
+          <p>
+            we are committed to safeguarding your privacy. When you use our
+            services, we collect necessary personal information, such as your
+            name, email address, and shipping details, to process orders and
+            enhance your user experience.
+            <hr></hr>
+            Additionally, non-personal information, like browser type and IP
+            address, is collected for analytical purposes
+            <hr></hr>
+            We do not sell or trade your personal information to third parties
+            without your consent.
           </p>
         </div>
       </div>
