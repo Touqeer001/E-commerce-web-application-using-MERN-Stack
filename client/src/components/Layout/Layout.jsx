cb0eb95b0fa6b1566b40f3136e0a01385538eb5c
Touqeer Ansari <touqeeransari001@gmail.com>
2024-01-31 01:23:19 +0530

    Updated

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,22 +1,38 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Toaster } from "react-hot-toast";
 
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import {Helmet} from "react-helmet";
 
-const Layout = (props) => {
+const Layout = (props,children, title, description, keywords, author) => {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
       <Header />
       <main style={{ minHeight: "70vh" }}>
-        <ToastContainer />
+      <Toaster />
 
         {props.children}
       </main>
       <Footer />
     </>
   );
+};
+
+Layout.defaultProps = {
+  title: "Ecommerce app - shop now",
+  description: "mern stack project",
+  keywords: "mern,react,node,mongodb",
+  author: "Techinfoyt",
 };
 
 export default Layout;
