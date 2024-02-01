5bbab711e7683ef8fbe6d01c5d15ae6022f9aedf
Touqeer Ansari <touqeeransari001@gmail.com>
2024-02-02 00:08:46 +0530

    updated

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout/Layout";
 
 const About = () => {
   return (
-    <Layout>
+    <Layout title={'About Us'}>
       <div className="row contactus" id="image">
         <div className="col-md-6 ">
           <h1 style={{ color: "white" }}> All About us!</h1>
