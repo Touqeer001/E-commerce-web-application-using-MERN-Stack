5cf9046d0d50b82f695f7c386a19e5c981e1ec01
Touqeer Ansari <touqeeransari001@gmail.com>
2024-02-02 00:08:53 +0530

    updated header.js

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import { useAuth } from "../context/auth";
 
 const Home = () => {
+  const [auth, setAuth] = useAuth();
   return (
     <Layout>
       <h1 style={{ color: "white" }}>Home Page.Coming Soon..</h1>
+      <pre style={{ color: "white" }}>{JSON.stringify(auth, null, 4)}</pre>
     </Layout>
   );
 };
