93a65bd144b0840d18b8026570b2b15e795e7e46
Touqeer Ansari <touqeeransari001@gmail.com>
2024-02-03 22:35:46 +0530

    admin dashboard

--- client/src/pages/Admin/AdminDashbord.jsx
+++ client/src/pages/Admin/AdminDashbord.jsx
@@ -1,21 +1,10 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
-import auth from "../../firebase.init";
+
 import AdminMenu from "../../components/Layout/AdminMenu";
-import { useAuthState } from "react-firebase-hooks/auth";
 
 export const AdminDashbord = () => {
-  const [userAuth] = useAuthState(auth);
-  const truncateUsername = (username, maxLength) => {
-    if (!username) {
-      return ""; // or handle the case where username is null or undefined
-    }
-    if (username.length <= maxLength) {
-      return username;
-    } else {
-      return username.slice(0, maxLength) + "...";
-    }
-  };
+  
   return (
     <Layout>
       <div className="container-fluid m-3 p-3 dashboard">
@@ -28,9 +17,10 @@ export const AdminDashbord = () => {
               {/* <h3> Admin Name : {auth?.user?.name}</h3>
               <h3> Admin Email : {auth?.user?.email}</h3>
               <h3> Admin Contact : {auth?.user?.phone}</h3> */}
+
+              <h3>Adminnn</h3>
                  
-                 <h3>{`Admin Name:${truncateUsername(userAuth.displayName)}`}</h3>
-              <h3>{`Admin Email:${userAuth.email}`}</h3>
+                 
             </div>
           </div>
         </div>
