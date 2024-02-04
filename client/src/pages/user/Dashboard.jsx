be8178d3c8f8345136f52d2aebae99c92cf994c9
Touqeer Ansari <touqeeransari001@gmail.com>
2024-02-04 20:19:25 +0530

    User Dashboard

--- client/src/pages/user/Dashboard.jsx
+++ client/src/pages/user/Dashboard.jsx
@@ -1,24 +1,12 @@
+
 import React from "react";
 import Layout from "../../components/Layout/Layout";
-import auth from "../../firebase.init";
 import UserMenu from "../../components/Layout/UserMenu";
-import { useAuthState } from "react-firebase-hooks/auth";
-
+import { useAuth } from "../../context/auth";
 const Dashboard = () => {
-  const [userAuth] = useAuthState(auth);
-  
-  const truncateUsername = (username, maxLength) => {
-    if (!username) {
-      return ""; // or handle the case where username is null or undefined
-    }
-    if (username.length <= maxLength) {
-      return username;
-    } else {
-      return username.slice(0, maxLength) + "...";
-    }
-  };
+  const [auth] = useAuth();
   return (
-    <Layout>
+    <Layout title={"Dashboard - Ecommerce App"}>
       <div className="container-flui m-3 p-3 dashboard">
         <div className="row">
           <div className="col-md-3">
@@ -26,10 +14,9 @@ const Dashboard = () => {
           </div>
           <div className="col-md-9">
             <div className="card w-75 p-3">
-             
-              <h3>{`UserName:${truncateUsername(userAuth.displayName, 8)}`}</h3>
-              <h3>{`UserEmail:${userAuth.email}`}</h3>
-             
+              <h3>{auth?.user?.name}</h3>
+              <h3>{auth?.user?.email}</h3>
+              <h3>{auth?.user?.address}</h3>
             </div>
           </div>
         </div>
