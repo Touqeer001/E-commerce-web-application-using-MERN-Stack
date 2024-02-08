2d026532a72d5864bd402f9e944920f8195c9f7a
Touqeer Ansari <touqeeransari001@gmail.com>
2024-02-08 21:22:22 +0530

    home page for user

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,11 @@ import toast from "react-hot-toast";
 import { Prices } from "../components/Layout/Prices";
 
 import Layout from "../components/Layout/Layout";
+import { AiOutlineReload } from "react-icons/ai";
 import { useAuth } from "../context/auth";
+// import { useCart } from "../context/cart";
+
+import "../pages/Style/Homepage.css";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -23,7 +27,9 @@ const Home = () => {
   //get all cat
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("http://localhost:8080/api/v1/category/get-category");
+      const { data } = await axios.get(
+        "http://localhost:8080/api/v1/category/get-category"
+      );
       if (data?.success) {
         setCategories(data?.category);
       }
@@ -40,7 +46,9 @@ const Home = () => {
   const getAllProducts = async () => {
     try {
       setLoading(true);
-      const { data } = await axios.get(`http://localhost:8080/api/v1/product/get-product`);
+      const { data } = await axios.get(
+        `http://localhost:8080/api/v1/product/get-product`
+      );
       setLoading(false);
       setProducts(data.products);
     } catch (error) {
@@ -53,161 +61,203 @@ const Home = () => {
     // getTotal();
   }, []);
 
-  // useEffect(() => {
-  //   if (page === 1) return;
-  //   loadMore();
-  // }, [page]);
-  //load more
-  // const loadMore = async () => {
-  //   try {
-  //     setLoading(true);
-  //     const { data } = await axios.get(`http://localhost:8080/api/v1/product/product-list/${page}`);
-  //     setLoading(false);
-  //     setProducts([...products, ...data?.products]);
-  //   } catch (error) {
-  //     console.log(error);
-  //     setLoading(false);
-  //   }
-  // };
+  //getTOtal COunt
+  const getTotal = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:8080/api/v1/product/product-count"
+      );
+      setTotal(data?.total);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    // filter by cat
-    const handleFilter = (value, id) => {
-      let all = [...checked];
-      if (value) {
-        all.push(id);
-      } else {
-        all = all.filter((c) => c !== id);
-      }
-      setChecked(all);
-    };
-    useEffect(() => {
-      if (!checked.length || !radio.length) getAllProducts();
-    }, [checked.length, radio.length]);
-  
-    useEffect(() => {
-      if (checked.length || radio.length) filterProduct();
-    }, [checked, radio]);
+  useEffect(() => {
+    if (page === 1) return;
+    loadMore();
+  }, [page]);
+
+  //load more
+  const loadMore = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(
+        `http://localhost:8080/api/v1/product/product-list/${page}`
+      );
+      setLoading(false);
+      setProducts([...products, ...data?.products]);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  // filter by cat
+  const handleFilter = (value, id) => {
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
+  useEffect(() => {
+    if (!checked.length || !radio.length) getAllProducts();
+  }, [checked.length, radio.length]);
+
+  useEffect(() => {
+    if (checked.length || radio.length) filterProduct();
+  }, [checked, radio]);
+
+  //get filterd product
+  const filterProduct = async () => {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:8080/api/v1/product/product-filters",
+        {
+          checked,
+          radio,
+        }
+      );
+      setProducts(data?.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     // <Layout>
     //   <h1 style={{ color: "white" }}>Home Page.Coming Soon..</h1>
     //   <pre style={{ color: "white" }}>{JSON.stringify(auth, null, 4)}</pre>
     // </Layout>
-    
-      <Layout title={"ALl Products - Best offers "}>
-        {/* banner image */}
-        <img
-          // src="/images/banner.png"
-          className="banner-img"
-          alt="bannerimage"
-          width={"100%"}
-        />
-        {/* banner image */}
-        <div className="container-fluid row mt-3 home-page">
-          <div className="col-md-3 filters">
-            <h4 className="text-center"  style={{ color: "white" }} >Filter By Category</h4>
-            <div className="d-flex flex-column">
-              {categories?.map((c) => (
-                <Checkbox
-                  key={c._id}
-                  onChange={(e) => handleFilter(e.target.checked, c._id)}
-                >
-                  {c.name}
-                </Checkbox>
-              ))}
-            </div>
-            {/* price filter */}
-            <h4 className="text-center mt-4"  style={{ color: "white" }}>Filter By Price</h4>
-            <div className="d-flex flex-column">
-              {/* <Radio.Group onChange={(e) => setRadio(e.target.value)}>
-                {Prices?.map((p) => (
-                  <div key={p._id}>
-                    <Radio value={p.array}>{p.name}</Radio>
-                  </div>
-                ))}
-              </Radio.Group> */}
-            </div>
-            <div className="d-flex flex-column">
-              <button
-                className="btn btn-danger"
-                onClick={() => window.location.reload()}
+
+    <Layout title={"ALl Products - Best offers "}>
+      {/* banner image */}
+      <img
+        // src="/images/banner.png"
+        className="banner-img"
+        alt="bannerimage"
+        width={"100%"}
+      />
+      {/* banner image */}
+      <div className="container-fluid row mt-3 home-page">
+        <div className="col-md-3 filters">
+          <h4 className="text-center" style={{ color: "white" }}>
+            Filter By Category
+          </h4>
+          <div className="d-flex flex-column">
+            {categories?.map((c) => (
+              <Checkbox
+                key={c._id}
+                onChange={(e) => handleFilter(e.target.checked, c._id)}
+                style={{ color: "white" }}
               >
-                RESET FILTERS
-              </button>
-            </div>
+                {c.name}
+              </Checkbox>
+            ))}
           </div>
-          <div className="col-md-9 ">
-            <h1 className="text-center"  style={{ color: "white" }}>All Products</h1>
-            <div className="d-flex flex-wrap">
-              {products?.map((p) => (
-                <div className="card m-2" key={p._id}>
-                  <img
-                    src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
-                    className="card-img-top"
-                    alt={p.name}
-                  />
-                  <div className="card-body">
-                    <div className="card-name-price">
-                      <h5 className="card-title">{p.name}</h5>
-                      <h5 className="card-title card-price">
-                        {p.price.toLocaleString("en-US", {
-                          style: "currency",
-                          currency: "USD",
-                        })}
-                      </h5>
-                    </div>
-                    <p className="card-text ">
-                      {p.description.substring(0, 60)}...
-                    </p>
-                    <div className="card-name-price">
-                      <button
-                        className="btn btn-info ms-1"
-                        onClick={() => navigate(`/product/${p.slug}`)}
-                      >
-                        More Details
-                      </button>
-                      <button
-                        className="btn btn-dark ms-1"
-                        onClick={() => {
-                          // setCart([...cart, p]);
-                          localStorage.setItem(
-                            "cart",
-                            // JSON.stringify([...cart, p])
-                          );
-                          toast.success("Item Added to cart");
-                        }}
-                      >
-                        ADD TO CART
-                      </button>
-                    </div>
-                  </div>
+          {/* price filter */}
+          <h4 className="text-center mt-4" style={{ color: "white" }}>
+            Filter By Price
+          </h4>
+          <div className="d-flex flex-column">
+            <Radio.Group
+              onChange={(e) => setRadio(e.target.value)}
+              style={{ color: "white" }}
+            >
+              {Prices?.map((p) => (
+                <div key={p._id}>
+                  <Radio value={p.array}>{p.name}</Radio>
                 </div>
               ))}
-            </div>
-            {/* <div className="m-2 p-3">
-              {products && products.length < total && (
-                <button
-                  className="btn loadmore"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setPage(page + 1);
-                  }}
-                >
-                  {loading ? (
-                    "Loading ..."
-                  ) : (
-                    <>
-                      {" "}
-                      Loadmore <AiOutlineReload />
-                    </>
-                  )}
-                </button>
-              )}
-            </div> */}
+            </Radio.Group>
+          </div>
+          <div className="d-flex flex-column">
+            <button
+              className="btn btn-danger"
+              onClick={() => window.location.reload()}
+            >
+              RESET FILTERS
+            </button>
           </div>
         </div>
-      </Layout>
-    );
-  };
-  
- 
+        <div className="col-md-9 ">
+          <h1 className="text-center" style={{ color: "white" }}>
+            All Products
+          </h1>
+          <div className="d-flex flex-wrap">
+            {products?.map((p) => (
+              <div className="card m-2" key={p._id}>
+                <img
+                  src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <div className="card-name-price">
+                    <h5 className="card-title" style={{ color: "white" }}>
+                      {p.name}
+                    </h5>
+                    <h5 className="card-title card-price">
+                      {p.price.toLocaleString("en-US", {
+                        style: "currency",
+                        currency: "USD",
+                      })}
+                    </h5>
+                  </div>
+                  <p className="card-text ">
+                    {p.description.substring(0, 60)}...
+                  </p>
+                  <div className="card-name-price">
+                    <button
+                      className="btn btn-info ms-1"
+                      onClick={() => navigate(`/product/${p.slug}`)}
+                    >
+                      More Details
+                    </button>
+                    <button
+                      className="btn btn-dark ms-1"
+                      onClick={() => {
+                        // setCart([...cart, p]);
+                        localStorage.setItem(
+                          "cart"
+                          // JSON.stringify([...cart, p])
+                        );
+                        toast.success("Item Added to cart");
+                      }}
+                    >
+                      ADD TO CART
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="m-2 p-3">
+            {products && products.length < total && (
+              <button
+                className="btn loadmore"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setPage(page + 1);
+                }}
+              >
+                {loading ? (
+                  "Loading ..."
+                ) : (
+                  <>
+                    {" "}
+                    Loadmore <AiOutlineReload />
+                  </>
+                )}
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
 export default Home;
