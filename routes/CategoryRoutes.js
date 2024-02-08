43855178c4c3c9846e790248a73b9f1caf1aace1
Touqeer Ansari <touqeeransari001@gmail.com>
2024-02-08 21:23:42 +0530

    all category routes

--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -1,14 +1,42 @@
 import express from "express";
-import { createCategoryController } from "../Controllers/CategoryController.js";
-import firebaseAuthMiddleware from "../middleware/Authmidleware.cjs";
 const router = express.Router();
-
+import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
+import {
+  categoryControlller,
+  createCategoryController,
+  deleteCategoryController,
+  singleCategoryController,
+  updateCategoryController,
+} from "../Controllers/CategoryController.js";
 
 // create category
 router.post(
   "/create-category",
-  firebaseAuthMiddleware,
+  requireSignIn,
+  isAdmin,
   createCategoryController
+);
+
+//update category
+router.put(
+  "/update-category/:id",
+  requireSignIn,
+  isAdmin,
+  updateCategoryController
+);
+
+//getALl category
+router.get("/get-category", categoryControlller);
+
+// /single category
+router.get("/single-category/:slug", singleCategoryController);
+
+//delete category
+router.delete(
+  "/delete-category/:id",
+  requireSignIn,
+  isAdmin,
+  deleteCategoryController
 );
 
 export default router;
