4fb647c6a60f6c8cf4af112ff61be3db6be4e0da
Touqeer Ansari <touqeeransari001@gmail.com>
2024-02-12 23:23:54 +0530

    update server file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import Connection from "./database/db.js";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import categoryRoutes from "./routes/CategoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 
@@ -19,6 +20,7 @@ app.use(express.json());
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 app.use(express.json());
 app.use(bodyParser.json({ extended: true }));
