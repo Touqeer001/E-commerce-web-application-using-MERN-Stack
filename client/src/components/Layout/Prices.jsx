8c5c6ba73e8f2682681ed6b8afbf6f61bf9f17a3
Touqeer Ansari <touqeeransari001@gmail.com>
2024-02-27 19:47:55 +0530

    Search  by Price

--- client/src/components/Layout/Prices.jsx
+++ client/src/components/Layout/Prices.jsx
@@ -1,32 +1,32 @@
 export const Prices = [
   {
     _id: 0,
-    name: "₹0 to 19",
-    array: [0, 19],
+    name: "₹99 to 499",
+    array: [99, 500],
   },
   {
     _id: 1,
-    name: "₹20 to 39",
-    array: [20, 39],
+    name: "₹500 to 999",
+    array: [500, 999],
   },
   {
     _id: 2,
-    name: "₹40 to 59",
-    array: [40, 59],
+    name: "₹1000 to 1499",
+    array: [1000, 1499],
   },
   {
     _id: 3,
-    name: "₹60 to 79",
-    array: [60, 79],
+    name: "₹1500 to 1899",
+    array: [1500, 1899],
   },
   {
     _id: 4,
-    name: "₹80 to 99",
-    array: [80, 99],
+    name: "₹1900 to 1999",
+    array: [1900, 1999],
   },
   {
     _id: 4,
-    name: "₹100 or more",
-    array: [100, 9999],
+    name: "₹2000 or more",
+    array: [2000, 9999],
   },
 ];
