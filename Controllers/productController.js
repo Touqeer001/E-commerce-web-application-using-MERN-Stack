9037304bdc02b0254f3a8ed31fb208d709724832
Touqeer Ansari <touqeeransari001@gmail.com>
2024-03-03 22:34:02 +0530

    Product controller

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -1,4 +1,5 @@
 import productModel from "../Model/productModel.js";
+import categoryModel from "../Model/categoryModel.js";
 import slugify from "slugify";
 import fs from "fs";
 
@@ -297,6 +298,26 @@ export const realtedProductController = async (req, res) => {
       success: false,
       message: "error while geting related product",
       error,
+    });
+  }
+};
+
+// get prdocyst by catgory
+export const productCategoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({ slug: req.params.slug });
+    const products = await productModel.find({ category }).populate("category");
+    res.status(200).send({
+      success: true,
+      category,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      error,
+      message: "Error While Getting products",
     });
   }
 };
