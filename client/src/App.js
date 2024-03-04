07e6704df3602b054935cbd7c368ec2ae52853e3
Touqeer Ansari <touqeeransari001@gmail.com>
2024-03-04 20:41:21 +0530

    aap component

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import Search from "./pages/Search";
 import ProductDetails from "./pages/ProductDetails";
 import Categories from "./pages/Categories";
 import CategoryProduct from "./pages/CategoryProduct";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/product/:slug" element={<ProductDetails />} />
         <Route path="/categories" element={<Categories />} />
+        <Route path="/cart" element={<CartPage />} />
         <Route path="/category/:slug" element={<CategoryProduct/>} />
         <Route path="/register" element={<Register />}></Route>
         <Route path="/forgot-password" element={<ForgotPasssword />}></Route>
